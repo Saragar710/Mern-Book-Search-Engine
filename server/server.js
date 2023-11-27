46c170458d61db0d74e32bfe7040eb6bfad42d6c
Sara Garcia <saragca81@gmail.com>
2023-11-27 14:04:00 -0800

    started code

--- server/server.js
+++ server/server.js
@@ -4,12 +4,23 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+
+//const { typeDefs, resolvers } = require('./schemas);
 const app = express();
 const PORT = process.env.PORT || 3001;
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers
+// })
 
+
+//const startApolloServer = async () => {
+//   await Server.start();
+// }
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//app.use('/graphql' , expressMiddleware(server));
 
 
 // if we're in production, serve client/build as static assets
@@ -22,3 +33,4 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+//startApolloServer();
