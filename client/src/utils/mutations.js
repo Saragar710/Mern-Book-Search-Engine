f73d440e4f08c90cce8e8700ec7488dfacf2d911
Sara Garcia <saragca81@gmail.com>
2023-11-28 16:30:53 -0800

    mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,16 @@
+import {gql} from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation loginUser(username: String!, password: String!,) {
+    addUser(username: String!, email: String!, password: String!):
+    saveBook(input: BookInput!): User
+    removeBook(bookId: ID!): User
+}
+
+
+
+
+
+
+
+`
