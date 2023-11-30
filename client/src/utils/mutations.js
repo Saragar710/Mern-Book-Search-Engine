77cc6e82c0d594d80023b4066394aceafbdb176c
Sara Garcia <saragca81@gmail.com>
2023-11-30 07:53:43 -0800

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,12 +1,37 @@
 import {gql} from '@apollo/client';
 
 export const LOGIN_USER = gql`
-mutation loginUser(username: String!, password: String!,) {
-    addUser(username: String!, email: String!, password: String!):
-    saveBook(input: BookInput!): User
-    removeBook(bookId: ID!): User
-};
-export const Ad
- 
-
+{ /*// mutation loginUser(username: String!, password: String!,) {
+    //     addUser(username: String!, email: String!, bookCount: Int!, savedBooks: []):
+    //     saveBook(input: BookInput!): User
+    //     removeBook(bookId: ID!): User
+// */}
+mutation loginUser($username: String!, $email: String, $bookCount: Int!, $ savedBook: []) {
+    loginUser(username: $username, email: $email, bookCount: $bookCount, savedBook: $savedBook) {
+        _id
+        username
+        email
+        bookCount
+        savedBook
+    }
+}
+`;
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!, $savedBook: []) {
+    addUser(username: $username, email: $email, password: $password, savedBook: $savedBook) {
+        _id
+        username
+        email
+        password
+        savedBook
+    }
+}
+`;
+export SAVE_BOOK = gql`
+ mutation saveBook($description: String!, $bookId: String!, $)
 `
+
+// export const ADD_USER = gql`
+// mutation addUser(_id: String!, usename: String!, password: String!, bookCount: Int,saveBook: Int, ) 
+
+// `
