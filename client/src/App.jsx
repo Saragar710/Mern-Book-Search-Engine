05326d618b9b5fea86f0a0c12c0c02bef12c6e8d
Sara Garcia <saragca81@gmail.com>
2023-11-29 16:39:11 -0800

    updated code in app,auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  //createHttpLink,
+} from '@apollo/client';
+//import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
